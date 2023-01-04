bc2e3588baf283ccab281684856cd608a05433ca
SaurabhKumar <srbhkumar624@gmail.com>
2023-01-04 21:18:31 +0530

    added file

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const jsonServer=require("json-server");
 const server=jsonServer.create();
 const router=jsonServer.router("db.json");
 const middlewares=jsonServer.defaults();
-const port=8221;
+const port=process.env.PORT||8221;
 
 server.use(middlewares);
 server.use(router);
